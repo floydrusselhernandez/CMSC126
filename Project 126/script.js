5f4896cb8d519755d5b436984c04f2eac210aff5
Rainer Mayagma <91443389+R4inMeka@users.noreply.github.com>
2023-05-01 12:02:16 +0800

    Add files via upload

--- Project 126/script.js
+++ Project 126/script.js
@@ -1,3 +1,32 @@
-function wishlist(){
-    
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
 }
+
+var btn_id;
+var parentElement;
+var id;
+
+const onClick = (event) => {
+  btn_id = event.target.id;
+  if (event.target.nodeName === 'BUTTON') {
+    console.log(event.target.id);
+  }
+}
+window.addEventListener('click', onClick);
+
+function closePopup(response) {
+  document.getElementById("popup").style.display = "none";
+  parentElement = document.getElementById(btn_id).parentElement.parentElement.parentElement.parentElement;
+  id = parentElement.id;
+  if (response) {
+    removeElement()
+  } else {
+
+  }
+}
+
+function removeElement() {
+  const element = document.getElementById(id);
+  element.remove();
+}
+
