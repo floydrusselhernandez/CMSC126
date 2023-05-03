aa612cd372ea6f9445a786eb872844d3e9c0e103
Rainer Mayagma <91443389+R4inMeka@users.noreply.github.com>
2023-05-03 16:02:08 +0800

    Add files via upload

---
+++ deliveryDropdown/dropdown.js
@@ -0,0 +1,9 @@
+function toggleDiv() {
+    var div = document.getElementById("address-fields");
+    if (div.style.display == "none") {
+      div.style.display = "flex";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  
