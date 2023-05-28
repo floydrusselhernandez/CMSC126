fd025ca91663a4a85de2b15ceef35b576907df83
Rainer Mayagma <rtmayagma@up.edu.ph>
2023-05-28 14:29:38 +0800

    Wishlist CSS changes
    Change footer, some css,

---
+++ php/footer.php
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<footer class="main-footer">
+    <div class="footer-logo">
+        <a href="#top" class="footer-logo-link">
+            <img id="Logo_White" src="res/Logo_White.png">
+            <img id="AnimaniacTxt_White" src="res/AnimaniacTxt_White.png">
+        </a>
+    </div>
+    <div class="footer-info">
+        <p class="footer-copyright">
+            &copy; 2023 - Animaniac. All rights reserved.
+        </p>
+        <p class="footer-maker">
+            Website made by: Hernandez, Mayagma, and Porras
+        </p>
+    </div>
+</footer>
