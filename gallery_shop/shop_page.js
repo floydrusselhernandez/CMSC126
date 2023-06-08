620bb2194bffbfc371bcaccfc23b0184a3e78526
Floyd Russel Hernandez <77273020+floydrusselhernandez@users.noreply.github.com>
2023-06-08 11:05:04 +0800

    Co-authored-by: belt <beltjunabisado@gmail.com>

---
+++ gallery_shop/shop_page.js
@@ -0,0 +1,10 @@
+
+function handleChangeAddress () {
+    let locationDOM = document.getElementById("location");
+    locationDOM.className = "hide-item"
+
+    let locationInputDOM = document.getElementById('location-input')
+    locationInputDOM.className = ""
+}
+let address = "Iloilo, Miagao, Bagumbayan";
+console.log("testing");
